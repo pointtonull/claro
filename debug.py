16d166ed3f6a528b9c875f7dd5c186ef257dd1ef
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-06-19 23:03:35 -0300

    nueva versión

--- debug.py
+++ debug.py
@@ -4,12 +4,12 @@ import time
 import sys
 
 INICIO = time.time()
+VERBOSE = 1
 
 def debug(*args):
-    global INICIO
-
-    sys.stderr.writelines("".join(
-        ["%7.2f" % (time.time() - INICIO),
-        " ",
-        " ".join([str(e) for e in args]) + "\n",
-        ]))
+    if VERBOSE:
+        sys.stderr.writelines("".join(
+            ["%7.2f" % (time.time() - INICIO),
+            " ",
+            " ". join([str(e) for e in args]) + "\n",
+            ])) 
